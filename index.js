a5599c8aec23f6ea6279dab5d8a4105b5b51c9a3
John-Kimani <kimwaweru100@gmail.com>
2022-12-04 12:02:30 +0300

    App: create node express server

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const cors = require("cors")
+
+const app = express()
+
+app.use(express.json())
+
+app.use(cors())
+
+app.get('/', (request, response) => {
+    response.send("Welcome to TamuFoods API...")
+})
+
+// Products
+app.get('/products', (request, response) => {
+    response.send([2, 3, 4])
+})
+
+const port = process.env.PORT || 5000
+
+app.listen(port, console.log(`Server running on port ${port}`))
